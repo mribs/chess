f1aaff47e655b8d56388a97fb18edb89b133c63f
mribs <maggieribble@gmail.com>
2024-10-27 01:46:25 -0600

    general frustration with the changes for this semester

---
+++ server/src/main/java/dataAccess/DAO/AuthDAO.java
@@ -0,0 +1,45 @@
+package dataAccess.DAO;
+
+
+import dataAccess.DataAccessException;
+import dataAccess.Database;
+import dataAccess.UnauthorizedException;
+import dataAccess.models.AuthToken;
+
+
+import java.util.UUID;
+
+public class AuthDAO {
+//creates a new authToken object
+  public AuthToken createToken(String username) throws DataAccessException {
+    String authTokenString = UUID.randomUUID().toString();
+    AuthToken newAuthToken = new AuthToken(username, authTokenString);
+
+    Database.authTokenMap.put(authTokenString, newAuthToken);
+    return newAuthToken;
+  }
+
+  //reads an authToken object
+  public AuthToken readToken(String authToken) {
+    return Database.authTokenMap.get(authToken);
+  }
+//update an authToken object
+  public void updateToken(String currAuthToken, AuthToken newAuthToken) throws UnauthorizedException {
+    if (Database.authTokenMap.containsKey(currAuthToken)) {
+      Database.authTokenMap.put(currAuthToken, newAuthToken);
+    }
+    else throw new UnauthorizedException();
+  }
+//delete an authToken object
+  public AuthToken deleteToken(String authToken) throws UnauthorizedException {
+    AuthToken oldAuthToken = Database.authTokenMap.remove(authToken);
+    if (oldAuthToken == null) {
+      throw new UnauthorizedException();
+    }
+    return oldAuthToken;
+  }
+
+  public void clearTokens() {
+    Database.authTokenMap.clear();
+  }
+}
---
+++ server/src/main/java/dataAccess/DAO/UserDAO.java
@@ -0,0 +1,39 @@
+package dataAccess.DAO;
+
+import dataAccess.AlreadyTakenException;
+import dataAccess.BadRequestException;
+import dataAccess.DataAccessException;
+import dataAccess.Database;
+import dataAccess.models.User;
+
+
+public class UserDAO {
+  //creates new user
+  public void createUser(User u) throws AlreadyTakenException, BadRequestException {
+    if (u == null || u.getUsername() == null) throw new BadRequestException();
+    if (Database.userMap.containsKey(u.getUsername()) ) throw new AlreadyTakenException();
+
+    Database.userMap.put(u.getUsername(), u);
+  }
+
+  //returns user information
+  public User readUser(String userName) throws DataAccessException {
+    if (userName == null) throw new DataAccessException("userName must not be null");
+
+    return Database.userMap.get(userName);
+  }
+
+  //updates user information
+//  void updateUser(User u) throws DataAccessException{
+//  }
+
+  //deletes user
+  void deleteUser(User u) throws DataAccessException {
+    Database.userMap.remove(u);
+  }
+
+  //clear users
+  public void clearUsers() {
+    Database.userMap.clear();
+  }
+}
