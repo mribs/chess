c59ea6731b05a3d6650db27934a9aff9d86a7f28
mribs <maggieribble@gmail.com>
2024-10-31 15:58:27 -0600

    shared models

---
+++ shared/src/main/java/model/AuthData.java
@@ -0,0 +1,6 @@
+package model;
+
+public class AuthData {
+  String authToken;
+  String username;
+}
---
+++ shared/src/main/java/model/GameData.java
@@ -0,0 +1,11 @@
+package model;
+
+import chess.ChessGame;
+
+public class GameData {
+  int gameID;
+  String whiteUsername;
+  String blackUsername;
+  String gameName;
+  ChessGame game;
+}
