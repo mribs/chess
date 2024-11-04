7d3622bfc2ca2b47db406511f1499b24b0e5d8ee
mribs <maggieribble@gmail.com>
2024-11-03 22:51:39 -0700

    setting up database initially

--- server/src/main/java/dataaccess/DatabaseManager.java
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -28,6 +28,8 @@ public class DatabaseManager {
                 var host = props.getProperty("db.host");
                 var port = Integer.parseInt(props.getProperty("db.port"));
                 CONNECTION_URL= String.format("jdbc:mysql://%s:%d", host, port);
+
+                configureDatabase();
             }
         } catch (Exception ex) {
             throw new RuntimeException("unable to process db.properties. " + ex.getMessage());
