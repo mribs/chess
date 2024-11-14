09fd529f439d873c14e2d45ca01a35385dc76df7
mribs <maggieribble@gmail.com>
2024-11-13 23:42:14 -0700

    code quality

--- server/src/main/java/service/services/JoinGameService.java
+++ server/src/main/java/service/services/JoinGameService.java
@@ -8,30 +8,35 @@ import service.results.JoinGameResult;
 
 public class JoinGameService {
   JoinGameRequest joinGameRequest;
-
   //return request result
   public JoinGameResult join(JoinGameRequest joinGameRequest, String username) throws DataAccessException, AlreadyTakenException, BadRequestException {
     GameDAO gameDAO = new GameDAO();
     Integer gameID = joinGameRequest.getGameID();
     //see if game exists
-    if (gameID == null) throw new BadRequestException();
+    if (gameID == null) {
+      throw new BadRequestException();
+    }
     Game game = gameDAO.find(gameID);
-    if (game == null) throw new BadRequestException();
-
+    if (game == null) {
+      throw new BadRequestException();
+    }
     //add user where relevant
     String playerColor = joinGameRequest.getPlayerColor();
     if ("WHITE".equals(playerColor)) {
-      if (game.getWhiteUsername() != null) throw new AlreadyTakenException();
+      if (game.getWhiteUsername() != null) {
+        throw new AlreadyTakenException();
+      }
       game.setWhiteUsername(username);
     }
     else if ("BLACK".equals(playerColor)) {
-      if (game.getBlackUsername() != null) throw new AlreadyTakenException();
+      if (game.getBlackUsername() != null) {
+        throw new AlreadyTakenException();
+      }
       game.setBlackUsername(username);
     }
     else {
       throw new BadRequestException();
     }
-
     //update game
     gameDAO.claimSpot(gameID, game);
     return new JoinGameResult(gameID, playerColor);
