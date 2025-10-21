0fddbe2a9843c92ace12a6192bcb41cac5bcfb0b
mribs <maggieribble@gmail.com>
2025-10-20 23:42:45 -0600

    stub gameDAO (incorrectly mostly)

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,4 +1,17 @@
 package dataaccess;
 
 public class GameDAO {
+    //   FIXME:add correct returns and fix variable types
+    public void createGame(String gameName) {
+    }
+
+    public void getGame(String gameID) {
+    }
+
+    public void listGames() {
+    }
+
+    public void updateGame(String gameID, String game) {
+    }
+
 }
