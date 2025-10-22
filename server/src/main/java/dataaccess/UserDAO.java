3b4305773baed5dff727e47fe90f845aae26b307
mribs <maggieribble@gmail.com>
2025-10-21 21:41:49 -0600

    stub userDAO (incorrectly mostly)

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,4 +1,10 @@
 package dataaccess;
 
 public class UserDAO {
+    //    TODO: correct return types and parameters
+    public void createUser(String username, String password) {
+    }
+
+    public void getUser(String username) {
+    }
 }
