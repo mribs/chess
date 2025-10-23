f5afeee8a6623e0aa467aaf329617abde6f72c94
mribs <maggieribble@gmail.com>
2025-10-22 21:48:45 -0600

    made it to the end of the instructions

---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,14 @@
+package service;
+
+import service.result.LoginResult;
+
+public class UserService {
+    public LoginResult register(RegisterRequest registerRequest) {
+    }
+
+    public LoginResult login(LoginRequest loginRequest) {
+    }
+
+    public void logout(LogoutRequest logoutRequest) {
+    }
+}
