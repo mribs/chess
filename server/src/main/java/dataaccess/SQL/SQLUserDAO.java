e0a52ce66cc70e7ac6b8d2daa84d28424c282538
mribs <maggieribble@gmail.com>
2025-10-28 21:06:24 -0600

    first sql class stubbed

---
+++ server/src/main/java/dataaccess/SQL/SQLUserDAO.java
@@ -0,0 +1,21 @@
+package dataaccess.SQL;
+
+import dataaccess.UserDAO;
+import model.UserData;
+
+public class SQLUserDAO implements UserDAO {
+    @Override
+    public void createUser(UserData userData) {
+        
+    }
+
+    @Override
+    public UserData getUser(String username) {
+        return null;
+    }
+
+    @Override
+    public void clearUsers() {
+
+    }
+}
