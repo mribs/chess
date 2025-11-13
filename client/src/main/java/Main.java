1b9c6f8b650cc6f4f28325d955fa6dd73f1a4796
mribs <maggieribble@gmail.com>
2025-11-12 23:40:33 -0700

    nonsense

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,6 +1,7 @@
 import chess.*;
 
 public class Main {
+    //    change for a commit for the day so that i can turn it in tomorrow if i want to
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
