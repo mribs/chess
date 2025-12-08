63e62e063b402a96f416b6bf771e1f1b6cb2e212
mribs <maggieribble@gmail.com>
2025-12-07 23:24:32 -0700

    stubbed connectionManage (copy from previous project)

---
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -0,0 +1,30 @@
+package server.websocket;
+
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
+
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConnectionManager {
+    public final ConcurrentHashMap<Integer, List<Connection>> games = new ConcurrentHashMap<>();
+
+    public void add(int gameID, String authToken, Session session) {
+    }
+
+    public void remove(int gameID, String authToken) {
+    }
+
+    public void sendConnectionFailure(String authToken, Session session, ServerMessage message) {
+    }
+
+    public void sendToRoot(int gameID, String authToken, ServerMessage message) {
+    }
+
+    public void broadcast(int gameID, String excludeAuthToken, ServerMessage notification) {
+    }
+
+    public void sendToAll(int gameID, ServerMessage message) {
+    }
+
+}
